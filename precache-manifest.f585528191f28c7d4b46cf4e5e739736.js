d361a85477134ae01707b11af80a960ab6917e3d
Felipe Marín Vásquez <pipemv@felipes-mbp.home>
2022-05-27 00:57:05 +1000

    Updates

---
+++ precache-manifest.f585528191f28c7d4b46cf4e5e739736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1eef2fc0197a9fc6bd19",
+    "url": "/film-library/static/css/main.489463a1.chunk.css"
+  },
+  {
+    "revision": "1eef2fc0197a9fc6bd19",
+    "url": "/film-library/static/js/main.7b473d29.chunk.js"
+  },
+  {
+    "revision": "949beeec5aa75578327a",
+    "url": "/film-library/static/js/runtime~main.af808f08.js"
+  },
+  {
+    "revision": "99271883fb96379cd468",
+    "url": "/film-library/static/js/2.1d61441b.chunk.js"
+  },
+  {
+    "revision": "ae04e687d04ed94bf6883444fa0ebab1",
+    "url": "/film-library/index.html"
+  }
+];
